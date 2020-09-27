6c7568b4e1f3ea05d5fab7aa9ec76ddaed033c58
mala23 <mail@mala23.me>
2020-09-27 13:39:02 +0200

    add back dirs from submodule

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `blog.colinschmid.net`,
+    author: `Colin Schmid`,
+    description: `A blog for my studies.`,
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/malaclipse23`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/mala23`,
+      },
+    ],
+  },
+}
---
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { Styled } from "theme-ui"
+
+export default function Bio() {
+  return (
+    <>
+    </>
+  )
+}
